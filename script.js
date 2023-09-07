2058eb352753b4e8fb5c2d4e350a88a0032759c9
Adeniji Victor Adeolu <adenijivictor99.va@gmail.com>
2023-09-07 13:12:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const today = document.getElementById('currentDayOfTheWeek')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Saturday"]
+
+const time = document.getElementById('currentUTCTime')
+
+time.innerHTML = new Date().getMilliseconds()
+
+
+
+
+const currentDayOfTheWeek = new Date().getDay();
+// const currentUTCTime = new Date();
+
+
+const slackUserName = window.localStorage.getItem("slackUserName");
+const githubURL = window.localStorage.getItem("githubURL");
+
+
+today.innerHTML = days[currentDayOfTheWeek]
+
+
+// displayTime();
+
+function displayTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    // Display the time in the format "hh:mm:ss"
+    document.getElementById("currentUTCTime").innerHTML = `${hours}:${minutes}:${seconds}`;
+
+    // Set a new timeout to call this function again in 1 second
+    setTimeout(displayTime, 1000);
+}
